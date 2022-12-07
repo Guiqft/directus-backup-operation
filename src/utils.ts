fdd9ced97fc7e194079cd7f3fd7954ffedc9170c
Guilherme de Oliveira <guilherme.oliveira@luizalabs.com>
2022-12-07 10:20:11 -0300

    feat: Add support for custom connection configuration

--- src/utils.ts
+++ src/utils.ts
@@ -1,7 +1,15 @@
-export const getFileName = () => {
+export const getFileName = (connection: IConnection) => {
     const date = new Date()
     const currentDate = `${date.getFullYear()}.${
         date.getMonth() + 1
     }.${date.getDate()}.${date.getHours()}.${date.getMinutes()}`
-    return `database-backup-${currentDate}.dump`
+    return connection.name
+        ? `${slug(connection.name)}-${currentDate}.dump`
+        : `database-backup-${currentDate}.dump`
 }
+
+const slug = (test: string) =>
+    test
+        .toLowerCase()
+        .replace(/ /g, "-")
+        .replace(/[^\w-]+/g, "")
